7a33af78ad1fc9e158808f251ba18333e24bc2fe
J. Choi <105338611+jngwk@users.noreply.github.com>
2024-07-24 11:28:02 +0000

    Sync LeetCode submission - Search Insert Position (java)

--- my-folder/problems/search_insert_position/solution.java
+++ my-folder/problems/search_insert_position/solution.java
@@ -4,16 +4,27 @@ class Solution {
         int rp = nums.length - 1;
 
         while(lp <= rp){
-            int mid = (lp + rp) / 2;
-            if(nums[mid] < target){
-                lp = mid + 1;
-            }else if(nums[mid] > target){
-                rp = mid - 1;
-            }else{
-                return mid;
+            int k = (rp + lp) / 2;
+            if(target < nums[k]){
+                rp = k - 1;
+            }
+            else if(target > nums[k]){
+                lp = k + 1;
+            }
+            else{
+                return k;
             }
         }
 
         return lp;
+        // if(target < nums[lp]){
+        //     if(lp == 0){
+        //         return lp;
+        //     }
+        //     return lp - 1;
+        // }
+        // else{
+        //     return lp + 1;
+        // }
     }
 }
